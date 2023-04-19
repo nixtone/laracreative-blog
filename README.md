98cd8772d7abd1968503a1df2fa63c21dbd588e4
Nixtone <nixtone@nxt.ru>
2023-04-19 17:18:07 +0300

    2. Первоначальный план и создаем миграции

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 [Плейлист](https://www.youtube.com/watch?v=UqlVcp21X7c&list=PLd2_Os8Cj3t8StX6GztbdMIUXmgPuingB&index=1)
 
-Стоп: [1. Подключаем bootstrap, auth и базу данных](https://www.youtube.com/watch?v=sdL_3z5gbjw&list=PLd2_Os8Cj3t8StX6GztbdMIUXmgPuingB&index=4)
+Стоп: [2. Первоначальный план и создаем миграции](https://www.youtube.com/watch?v=KKFwLAklnzA&list=PLd2_Os8Cj3t8StX6GztbdMIUXmgPuingB&index=3)
 
 ### 1. Подключаем bootstrap, auth и базу данных  
 ```sh  
@@ -10,4 +10,13 @@ composer require laravel/ui
 php artisan ui bootstrap  
 php artisan ui:auth  
 npm install && npm run dev  
+```  
+
+### 2. Первоначальный план и создаем миграции  
+```sh  
+php artisan make:model Post -m  
+php artisan make:model Category -m  
+php artisan make:model Tag -m  
+php artisan make:model PostTag -m  
+php artisan migrate  
 ```  
---
+++ database/migrations/2023_04_19_135722_create_post_tags_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('post_tags', function (Blueprint $table) {
+            $table->id();
+
+            $table->unsignedBigInteger('post_id');
+            $table->unsignedBigInteger('tag_id');
+
+            $table->timestamps();
+
+            $table->index('post_id', 'post_tag_post_idx');
+            $table->index('tag_id', 'post_tag_tag_idx');
+            $table->foreign('post_id', 'post_tag_post_fk')->on('categories')->references('id');
+            $table->foreign('tag_id', 'post_tag_tag_fk')->on('categories')->references('id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('post_tags');
+    }
+};
